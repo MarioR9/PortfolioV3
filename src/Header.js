60d0867bb0b8443619e6edc5706abcc5fde4305e
marior9 <mariorodriguezan@gmail.com>
2020-09-22 17:43:44 -0400

    nav bar responsve for mobile

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React , {useState} from 'react'
 import logo from './assets/Mlogo.png'
 import './Header.css';
 import MenuIcon from '@material-ui/icons/Menu';
-import IconButon from '@material-ui/core/IconButton'
-
+import HomeIcon from '@material-ui/icons/Home';
+import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
+import PersonIcon from '@material-ui/icons/Person';
+import IconButon from '@material-ui/core/IconButton';
 const Header = () => {
+    
     return (
+        <div>
         <div className="header">
+            <div>
             <IconButon>
             <img 
             className="header__logo"
@@ -14,10 +19,28 @@ const Header = () => {
             alt='portfolio logo'
             /> 
             </IconButon>
-            <IconButon>
-            <MenuIcon className="header__menu" fontSize="large"/>
-            </IconButon>
+            </div>
+            <div className="header__menu">
+                <div className='header__links'>
+                <IconButon>
+                <HomeIcon className="header__icon" fontSize="large"/>
+                </IconButon>
+                <IconButon>
+                <PersonIcon className="header__icon" fontSize="large"/>
+                </IconButon>
+                <IconButon>
+                <CloudDownloadIcon className="header__icon" fontSize="large"/>
+                </IconButon>
+                </div>
+            </div>
+            <div className="header__burger">
+                <IconButon>
+                <MenuIcon className="header__burger" fontSize="large"/>
+                </IconButon>
+            </div>
         </div>
+        </div>
+       
     )
 }
 
