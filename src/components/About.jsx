b51f27b85bfdc7caacc1e75d3c6f2693c9338992
marior9 <mariorodriguezan@gmail.com>
2020-09-23 17:29:08 -0400

    about info and custm img addded

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,36 +2,26 @@ import React from 'react'
 import '../css/About.css'
 import aboutImg from '../assets/about.png'
 
-import { Container,Row,Col } from 'react-bootstrap';
-
-
 export const About = () => {
     const about= "Hello, I'm Mario, I am a Full-stack web developer based on Washington, DC. I have expericen with multiple programing langues as well as data base. I have experice with: "
     return (
         <div className="about">
-           <Container>
-            <Row>
-            <Col>
             <img className='about__img' src={aboutImg}/>
-            </Col>
-            <Col>
+            <div className='about__info'>
             <h2>About Me</h2>
-            {about}
-            <ul>
-                <li>JavaScript</li>
-                <li>Ruby on Rails</li>
-                <li>React.js</li>
-                <li>Puppetter.js</li>
-                <li>Node.js</li>
+             {about}
+             <div className='about_tech'>
                 <li></li>
-                <li>React</li>
-                <li>React</li>
-
-            </ul>
-            </Col>
-            </Row>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+             </div>
+             <div className='about__donwload'>
+             <span className='about__donwloadBtn'>Download</span>
+             </div>
+            </div>
            
-            </Container>
         </div>
     )
 }
