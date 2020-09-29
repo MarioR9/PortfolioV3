d21b4bff4047afac7cb8780b9c46ed0b96936ae9
marior9 <mariorodriguezan@gmail.com>
2020-09-29 10:40:07 -0400

    basic modal set

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,6 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "../css/Portfolio.css";
+
 import translateme from "../assets/translateme.png";
 import rickandmorty1 from "../assets/rickMorty1.png";
 import tonester from "../assets/tonester.png";
@@ -9,15 +10,7 @@ import portfoliov2 from "../assets/portfoliov2.png";
 import marsrover from "../assets/marsrover.png";
 import portfoliov3 from "../assets/portfoliov3.png";
 
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { useTheme } from '@material-ui/core/styles';
-import { Container } from "react-bootstrap";
+import { Container, Row, Col, Modal, Button } from "react-bootstrap";
 
 const Portfolio = () => {
   const pages = [
@@ -30,23 +23,65 @@ const Portfolio = () => {
     marsrover,
     portfoliov3,
   ];
-
-  const [open, setOpen] = useState(false);
-  const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
- 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
   const data = {
-                translateme:{ Title: "Translateme", description: "Translate me uses a machine learning technology to classify and translate images into different languages. Once the image is captured, users can flip the image to reveal the desired translated word for the image.", technologies:['Javascript','React','RubyonRails', 'IBMWatson','Cloudinary','SemanticUI'],images:['https://raw.githubusercontent.com/MarioR9/TranslateMeFrontend/master/ReadmeImages/main.png','https://github.com/MarioR9/TranslateMeFrontend/raw/master/ReadmeImages/categories.png','https://github.com/MarioR9/TranslateMeFrontend/raw/master/ReadmeImages/categoryCards.png','https://github.com/MarioR9/TranslateMeFrontend/raw/master/ReadmeImages/selectLanguage.png']},
-                rickandMorty:{ Title: "rickandMorty", description: "", technologies:[],images:[]}, 
-              }
+    translateme: {
+      Title: "Translateme",
+      description:
+        "Translate me uses a machine learning technology to classify and translate images into different languages. Once the image is captured, users can flip the image to reveal the desired translated word for the image.",
+      technologies: [
+        "Javascript",
+        "React",
+        "RubyonRails",
+        "IBMWatson",
+        "Cloudinary",
+        "SemanticUI",
+      ],
+      images: [
+        "https://raw.githubusercontent.com/MarioR9/TranslateMeFrontend/master/ReadmeImages/main.png",
+        "https://github.com/MarioR9/TranslateMeFrontend/raw/master/ReadmeImages/categories.png",
+        "https://github.com/MarioR9/TranslateMeFrontend/raw/master/ReadmeImages/categoryCards.png",
+        "https://github.com/MarioR9/TranslateMeFrontend/raw/master/ReadmeImages/selectLanguage.png",
+      ],
+    },
+  };
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+
+  const body = (
+    <div>
+      <Container>
+        <Row>
+          <Col>
+            <h2>{data["translateme"].Title}</h2>
+            <p>{data["translateme"].description}</p>
+            <div className="portfolio__tech">
+              <Row>
+                {data["translateme"].technologies.map((tech) => (
+                  <li
+                    data-aos="fade-up"
+                    data-aos-delay="40"
+                    data-aos-easing="ease-in-out"
+                    data-aos-offset="100"
+                    data-aos-duration="1800"
+                    key={tech}
+                  >
+                    <span>{tech}</span>
+                  </li>
+                ))}
+              </Row>
+            </div>
+          </Col>
+          <Col>
+            {data["translateme"].images.map((img) => (
+              <img className="portfolio__img" src={img} />
+            ))}
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+
   return (
     <div id="portfolio">
       <Container
@@ -57,26 +92,64 @@ const Portfolio = () => {
         data-aos-duration="1200"
       >
         <Container>
-        <Dialog
-        fullScreen={fullScreen}
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="responsive-dialog-title"
-      >
-        <DialogTitle id="responsive-dialog-title">{"Use Google's location service?"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            contente Here
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-         
-          <Button onClick={handleClose} color="primary" autoFocus>
-            Agree
-          </Button>
-        </DialogActions>
-        </Dialog>
+          <Modal
+            size="xl"
+            centered
+            enforceFocus
+            show={show}
+            onHide={handleClose}
+            className="portfolio__modalContainer"
+          >
+            <Modal.Header closeButton></Modal.Header>
+            <Modal.Body centered scrollable>
+              <Container>
+                <Row>
+                  <Col>
+                    <h2>{data["translateme"].Title}</h2>
+                    <p>{data["translateme"].description}</p>
+                    <div className="portfolio__tech">
+                      <Row>
+                        {data["translateme"].technologies.map((tech) => (
+                          <li
+                            data-aos="fade-up"
+                            data-aos-delay="40"
+                            data-aos-easing="ease-in-out"
+                            data-aos-offset="100"
+                            data-aos-duration="1800"
+                            key={tech}
+                          >
+                            <span>{tech}</span>
+                          </li>
+                        ))}
+                      </Row>
+                    </div>
+                    <div>
+                      <ul className="portfolio__social">
+                        <li>
+                          <a href="https://github.com/MarioR9/TranslateMeFrontend">
+                            <i className="fab fa-github fa-lg"></i>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="https://www.youtube.com/watch?v=M7LLMVlS6hg&feature=emb_logo">
+                            <i class="fab fa-youtube fa-lg"></i>
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </Col>
+                  <Col>
+                    {data["translateme"].images.map((img) => (
+                      <img className="portfolio__img" src={img} />
+                    ))}
+                  </Col>
+                </Row>
+              </Container>
+            </Modal.Body>
+            <Modal.Footer></Modal.Footer>
+          </Modal>
         </Container>
+
         <div className="portfolio">
           <div className="portfolio__header">
             <h3>Recent Projects</h3>
@@ -91,7 +164,12 @@ const Portfolio = () => {
                 data-aos-duration="1200"
                 key={page}
               >
-                <img onClick={handleClickOpen} className="portfolio__img" src={page} alt={page} />
+                <img
+                  onClick={() => setShow(true)}
+                  className="portfolio__img"
+                  src={page}
+                  alt={page}
+                />
               </ul>
             ))}
           </div>
