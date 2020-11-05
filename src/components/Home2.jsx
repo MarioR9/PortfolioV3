c08188754f9fa4ce8fc241979ab514ab2cb2209e
Mario Rodriguez <31521338+MarioR9@users.noreply.github.com>
2020-11-04 23:50:03 -0500

    Create Home2.jsx

---
+++ src/components/Home2.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import '../css/Home.css';
+
+const Home2 = () => {
+	return (
+		<div className="home">
+		
+		</div>
+	);
+};
+export default Home;
